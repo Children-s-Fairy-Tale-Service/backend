72ac9d0eb99a8a9d13e6d2624a44d188c1de23b4
kimhotae9270 <138718925+kimhotae9270@users.noreply.github.com>
2025-12-01 03:26:50 +0900

    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(
 )
 
 origins = [
-    "https://fairytale-hotaes-projects-9b3c092d.vercel.app/",
+    "https://fairytale-pied.vercel.app",
     "http://localhost:5173",  # React 기본 포트
     "http://127.0.0.1:5173",
 
